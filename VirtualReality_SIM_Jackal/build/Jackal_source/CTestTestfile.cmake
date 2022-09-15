5b1bc5bb48f317b273d52a2352df746c02f01cb2
MahmoudYidi <dopemubiks@gmail.com>
2022-09-15 15:03:32 +0100

    Jackal Robot with HTC VR

---
+++ VirtualReality_SIM_Jackal/build/Jackal_source/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/axel/mahmoud_ws/src/Jackal_source
+# Build directory: /home/axel/mahmoud_ws/build/Jackal_source
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ VirtualReality_SIM_Jackal/build/jackal/jackal_msgs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/axel/mahmoud_ws/src/jackal/jackal_msgs
+# Build directory: /home/axel/mahmoud_ws/build/jackal/jackal_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
